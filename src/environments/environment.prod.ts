fe8fbbc7a783434bb16e39a2e230e753e0cc1e62
Ananya Shukla <ananyashukla0307@gmail.com>
2026-05-13 11:46:32 +0530

    Fix frontend API environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://parkease-api-gateway.onrender.com/api/v1',
+  apiUrl: 'https://parkease-auth-service-1tz8.onrender.com/api/v1',
   googleClientId: '868629147452-m7mfalga62f9apliiqttrqi51ise8dps.apps.googleusercontent.com'
 };
